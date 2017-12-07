b260da983640e3320d862d9b052dabef1712345e
hamid <hm2123@email.vccs.edu>
2017-12-07 10:50:02 -0500

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+/**
+ * Creator: Hamid Majeed
+ */
+
+public class Driver {
+
+    public static void main(String[] args) {
+        GUI sum1= new GUI();
+        sum1.setVisible(true);
+
+    }
+
+}
